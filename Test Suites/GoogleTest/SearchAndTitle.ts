fc52a07d4f4c61934b85c99f10ab57dd334545ab
kumarsushobhan <kumarsushobhan@sushobhans-mbp>
2019-02-06 23:27:56 +0530

    Added 2 test cases 1) Search 2) Title

---
+++ Test Suites/GoogleTest/SearchAndTitle.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SearchAndTitle</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>439df4c6-1d05-480d-90db-87ee74210a0e</testSuiteGuid>
+   <testCaseLink>
+      <guid>8ce3b650-0bee-45e7-b8a5-83d573b1d118</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GoogleSearch/Title</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eb9a4d8b-4e3e-437a-ab86-03ac7734151b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GoogleSearch/Search</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
