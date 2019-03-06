84903d200062a7841695a9c5cf6fb5c59a1c92fb
ks397v <ks397v@att.com>
2019-03-06 17:15:51 +0530

    Updated the test suite

--- Test Suites/GoogleTest/SearchAndTitle.ts
+++ Test Suites/GoogleTest/SearchAndTitle.ts
@@ -11,15 +11,15 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>439df4c6-1d05-480d-90db-87ee74210a0e</testSuiteGuid>
    <testCaseLink>
-      <guid>8ce3b650-0bee-45e7-b8a5-83d573b1d118</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/GoogleSearch/Title</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>eb9a4d8b-4e3e-437a-ab86-03ac7734151b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/GoogleSearch/Search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8ce3b650-0bee-45e7-b8a5-83d573b1d118</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GoogleSearch/Title</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
